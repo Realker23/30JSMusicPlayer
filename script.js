619010142569fdb6da2c145057a5827600fb19ac
Abhishek Roushan <abhishekashu23@gmail.com>
2023-08-30 21:32:59 +0530

    v1.1

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const song = document.getElementById("song");
 const progress = document.getElementById("progress");
 const playIcon = document.getElementById("playIcon");
+const songName = document.getElementById("songName");
+const authorName = document.getElementById("authorName");
+const image = document.querySelector("img");
+
+const musicList = [{
+                    Name : "Despacito",
+                    author : "Luis Fonsi ft. Puerto Rican"
+                },
+                {
+                    Name : "Maan meri jaan",
+                    author : "King" 
+                },
+                {
+                    Name : "Tere Pyar Mein",
+                    author : "Arjit Singh"
+                }];
 
 song.onloadedmetadata = function(){
     progress.max = song.duration;
@@ -19,6 +35,48 @@ function playPause(){
         playIcon.classList.remove("fa-play");
         playIcon.classList.add("fa-pause");
     }
+}
+
+function nextPreviousPlayPause(){
+    if(playIcon.classList.contains("fa-pause")){
+        song.play();
+    }else{
+        song.pause();
+    }
+}
+
+let musicListIndex = 0;
+
+function playNext(){
+    musicListIndex++
+    if(musicListIndex>=musicList.length){
+        musicListIndex = 0;
+    }
+    
+    song.src = "media/" + musicList[musicListIndex].Name + ".mp3";
+    songName.innerText = musicList[musicListIndex].Name;
+    authorName.innerText = musicList[musicListIndex].author;
+    image.src = "media/" + musicList[musicListIndex].Name + ".jpg";
+    nextPreviousPlayPause();
+    
+    
+
+}
+
+function playPrevious(){
+    musicListIndex-- ; 
+    console.log(musicListIndex);
+    if(musicListIndex < 0){
+        musicListIndex = musicList.length -1;
+    }
+    song.src = "media/" + musicList[musicListIndex].Name + ".mp3";
+    songName.innerText = musicList[musicListIndex].Name;
+    authorName.innerText = musicList[musicListIndex].author;
+    image.src = "media/" + musicList[musicListIndex].Name + ".jpg";
+    nextPreviousPlayPause();
+    
+    
+
 }
 
 setInterval(()=>{
